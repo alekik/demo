1339547e8fa59a67dd319e18ea59b7fb7aeb73d1
alekik <leha.konst@gmail.com>
2022-05-16 00:45:18 +0400

    .

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -14,4 +15,24 @@ public class MainController {
     public String helloWorldController() {
         return "hi";
     }
+
+    @RequestMapping(value = "/login")
+    public String loginController() {
+        return "login";
+    }
+    @RequestMapping(value = "/log1")
+    public String logController() {
+        return "log1";
+    }
+
+//    @PostMapping("/login")
+//    public String check(@RequestParam String action) {
+//
+//        System.out.println(action);
+//
+//        if (action.equals("login")) {
+//            return "hi";
+//        }
+//        return "login";
+//    }
 }
